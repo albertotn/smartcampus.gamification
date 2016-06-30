c2a9fe3afaf87819d442bbc73901778b1edb8f27
Raffaele Calzà <raffaelecalza4@gmail.com>
2016-06-30 16:46:53 +0200

    Option for change graphs to actions and points charts and style players chart

--- game-engine.web/src/main/resources/consoleweb-assets/game-stats/game-stats.js
+++ game-engine.web/src/main/resources/consoleweb-assets/game-stats/game-stats.js
@@ -1,6 +1,6 @@
 angular.module('gamificationEngine.stats', [])
 
-.controller('StatsCtrl', function ($scope, $stateParams, $timeout, gamesFactory, statsFactory) {
+.controller('StatsCtrl', function ($scope, $stateParams, $timeout, $filter, gamesFactory, statsFactory) {
     $scope.alerts = {
         'loadGameError': false
     };;
@@ -21,20 +21,22 @@ angular.module('gamificationEngine.stats', [])
             },
             colors: [
                 {
-                    backgroundColor: 'rgba(148,159,177,0.2)',
-                    pointBackgroundColor: 'rgba(148,159,177,1)',
-                    pointHoverBackgroundColor: 'rgba(148,159,177,1)',
-                    borderColor: 'rgba(148,159,177,1)',
-                    pointBorderColor: '#fff',
-                    pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+                    backgroundColor: 'rgba(230, 126, 24,0.2)',
+                    pointBackgroundColor: 'rgba(230, 126, 24,1)',
+                    pointHoverBackgroundColor: 'rgba(230, 126, 24,1)',
+                    borderColor: 'rgba(230, 126, 24,1)',
+                    borderWidth: 4,
+                    pointBorderColor: 'rgba(230, 126, 24,1)',
+                    pointHoverBorderColor: 'rgba(230, 126, 24,0.8)'
       },
                 {
-                    backgroundColor: 'rgba(77,83,96,0.2)',
-                    pointBackgroundColor: 'rgba(77,83,96,1)',
-                    pointHoverBackgroundColor: 'rgba(77,83,96,1)',
-                    borderColor: 'rgba(77,83,96,1)',
-                    pointBorderColor: '#fff',
-                    pointHoverBorderColor: 'rgba(77,83,96,0.8)'
+                    backgroundColor: 'rgba(231,76,60,0.4)',
+                    pointBackgroundColor: 'rgba(231,76,60,1)',
+                    pointHoverBackgroundColor: 'rgba(231,76,60,1)',
+                    borderColor: 'rgba(231,76,60,1)',
+                    borderWidth: 4,
+                    pointBorderColor: 'rgba(231,76,60,1)',
+                    pointHoverBorderColor: 'rgba(231,76,60,1)'
       }
     ]
         },
@@ -74,6 +76,7 @@ angular.module('gamificationEngine.stats', [])
             }
         },
         points: {
+            type: 'pie',
             labels: [],
             data: [],
             options: {
@@ -84,6 +87,7 @@ angular.module('gamificationEngine.stats', [])
             }
         },
         actions: {
+            type: 'pie',
             labels: [],
             data:  [],
             options: {
@@ -134,67 +138,68 @@ angular.module('gamificationEngine.stats', [])
     });
 
     statsFactory.getFromRangeStats(334, 343).then(function (data) {
-        $scope.stats = data;
-        // Players chart
-        var array = [];
-        $scope.chartsData.players.series.push('Numero giocatori');
-        data.graphsData.totalPlayers.forEach(function (element, idx) {
-            $scope.chartsData.players.labels.push(element.date);
-            array.push(element.number);
-        });
-        $scope.chartsData.players.data.push(array);
-
-        array = [];
-        $scope.chartsData.players.series.push('Giocatori attivi');
-        data.graphsData.activePlayers.forEach(function (element, idx) {
-            array.push(element.number);
-        });
-        $scope.chartsData.players.data.push(array);
-
-        // Badge chart
-        data.graphsData.badgeCategories.forEach(function (element, idx) {
-            $scope.chartsData.badges.labels.push(element.name);
-            $scope.chartsData.badges.data[0].push(element.users);
-        });
-
-        // Points chart
-        data.graphsData.typeOfPoints.forEach(function (element, idx) {
-            $scope.chartsData.points.data.push(element.points);
-            $scope.chartsData.points.labels.push(element.name);
-        });
-
-        // Actions chart
-        data.graphsData.typeOfAtions.forEach(function (element, idx) {
-            $scope.chartsData.actions.data.push(element.matchs);
-            $scope.chartsData.actions.labels.push(element.name);
-        });
-
-        // Global result for challenges
-        $scope.chartsData.globalChallenges.series.push('Tutte le challenge');
-        $scope.chartsData.globalChallenges.data.push([]);
-        data.graphsData.globalChallengesStatus.forEach(function (element, idx) {
-            $scope.chartsData.globalChallenges.data[0].push(element.number);
-            $scope.chartsData.globalChallenges.labels.push(element.status);
-        });
-
-        // All Challenges
-        data.graphsData.typeOfChallenges.forEach(function (challenge, idx) {
-            $scope.chartsData.globalChallenges.series.push(challenge.name);
-            var data = [];
-            challenge.data.forEach(function (element, idx) {
-                data.push(element.number);
+            $scope.stats = data;
+            // Players chart
+            var array = [];
+            $scope.chartsData.players.series.push('Numero giocatori');
+            data.graphsData.totalPlayers.forEach(function (element, idx) {
+                $scope.chartsData.players.labels.push($filter('date')(element.date, "dd/MM/yyyy"));
+                array.push(element.number);
             });
-            $scope.chartsData.globalChallenges.data.push(data);
-        });
+            $scope.chartsData.players.data.push(array);
 
-        data.graphsData.matchsPerPlayers.forEach(function (element, idx) {
-            $scope.chartsData.matchPlayers.labels.push(element.range);
-            $scope.chartsData.matchPlayers.data[0].push(element.users);
-        });
+            array = [];
+            $scope.chartsData.players.series.push('Giocatori attivi');
+            data.graphsData.activePlayers.forEach(function (element, idx) {
+                array.push(element.number);
+            });
+            $scope.chartsData.players.data.push(array);
 
-    }, function () {
-        $scope.alerts.loadGameError = true;
-    });
+            // Badge chart
+            data.graphsData.badgeCategories.forEach(function (element, idx) {
+                $scope.chartsData.badges.labels.push(element.name);
+                $scope.chartsData.badges.data[0].push(element.users);
+            });
+
+            // Points chart
+            data.graphsData.typeOfPoints.forEach(function (element, idx) {
+                $scope.chartsData.points.data.push(element.points);
+                $scope.chartsData.points.labels.push(element.name);
+            });
+
+            // Actions chart
+            data.graphsData.typeOfAtions.forEach(function (element, idx) {
+                $scope.chartsData.actions.data.push(element.matchs);
+                $scope.chartsData.actions.labels.push(element.name);
+            });
+
+            // Global result for challenges
+            $scope.chartsData.globalChallenges.series.push('Tutte le challenge');
+            $scope.chartsData.globalChallenges.data.push([]);
+            data.graphsData.globalChallengesStatus.forEach(function (element, idx) {
+                $scope.chartsData.globalChallenges.data[0].push(element.number);
+                $scope.chartsData.globalChallenges.labels.push(element.status);
+            });
+
+            // All Challenges
+            data.graphsData.typeOfChallenges.forEach(function (challenge, idx) {
+                $scope.chartsData.globalChallenges.series.push(challenge.name);
+                var data = [];
+                challenge.data.forEach(function (element, idx) {
+                    data.push(element.number);
+                });
+                $scope.chartsData.globalChallenges.data.push(data);
+            });
+
+            data.graphsData.matchsPerPlayers.forEach(function (element, idx) {
+                $scope.chartsData.matchPlayers.labels.push(element.range);
+                $scope.chartsData.matchPlayers.data[0].push(element.users);
+            });
+
+        },
+        function () {
+            $scope.alerts.loadGameError = true;
+        });
 
     $scope.badgeCatClick = function (points, evt) {
         if (!points[0]) return;
